bad64fdd5dada7c0bb548d33585545cc3f8c6d3c
pablo-jm <pablojime1998@gmail.com>
2025-06-01 20:04:49 +0200

    Shop fixed

--- ArDe-Front/src/pages/Shop/Shop.jsx
+++ ArDe-Front/src/pages/Shop/Shop.jsx
@@ -1,21 +1,23 @@
 import Header from '../../components/Header/Header';
-import Footer from "../../components/Footer/Footer"
-import ContactSection from "../../components/ContactSection/ContactSection"
+import Footer from '../../components/Footer/Footer'
+import ContactSection from '../../components/ContactSection/ContactSection'
+import './Shop.css'
 
 const Shop = () => {
   return (
-    <div className='shop-father-container'>
+    <div className="shop-father-container">
       <Header />
         <main className="shop-content-wrapper">
-
-          <div className="catalogo-grid">
-            
+          <div className="shop-title">
+            <h3>Tienda</h3>
           </div>
+          <div className="catalogo-grid">
 
-          <div id="shop-contact-section">
-            <ContactSection />
           </div>
         </main>
+        <div id="shop-contact-section">
+            <ContactSection />
+          </div>
       <Footer />
     </div>
   );
